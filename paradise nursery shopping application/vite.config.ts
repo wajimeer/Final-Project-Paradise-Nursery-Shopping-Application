4ad0fdc463ce8ee9c906f99c71fbaae55015edff
Philipp <philipp.warnecke01@gmail.com>
2024-07-19 20:38:38 +0200

    Deploy

--- paradise nursery shopping application/vite.config.ts
+++ paradise nursery shopping application/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: "/Final-Project-Paradise-Nursery-Shopping-Application",
   plugins: [react()],
   server: {
     port: 5173,
