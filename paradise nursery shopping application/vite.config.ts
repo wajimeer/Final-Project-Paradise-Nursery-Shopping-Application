0636e5461e16f73309206768aaac097e920eb798
Philipp <philipp.warnecke01@gmail.com>
2024-07-19 22:54:40 +0200

    fixed Problems with base page likings

--- paradise nursery shopping application/vite.config.ts
+++ paradise nursery shopping application/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: "/Final-Project-Paradise-Nursery-Shopping-Application",
+  base: "/Final-Project-Paradise-Nursery-Shopping-Application/",
   plugins: [react()],
   server: {
     port: 5173,
